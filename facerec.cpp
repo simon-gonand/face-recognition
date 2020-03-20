5f4ff44cde4fa78ce888bb3da746f8a5806f2e60
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-03-20 19:59:14 +0000

    add video to the program

--- facerec.cpp
+++ facerec.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/core/types.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
   std::vector<int>     labels;
 
   // Iterate through all subdirectories, looking for .pgm files
-  fs::path p(argc > 1 ? argv[1] : "../att_faces");
+  fs::path p(argc > 1 ? argv[1] : "src/att_faces");
   for (const auto &entry : fs::recursive_directory_iterator{ p }) {
     if (fs::is_regular_file(entry.status())) { // Was once always (wrongly) false in VS
       if (entry.path().extension() == ".pgm") {
@@ -29,18 +30,52 @@ int main(int argc, char *argv[])
   }
 
   // Randomly choose an image, and remove it from the main collection
-  std::srand(std::time(0));
-  int rand_image_id = std::rand() % images.size();
+  /*std::srand(std::time(0));
+  int rand_image_id = 350;
   cv::Mat testSample = images[rand_image_id];
   int     testLabel  = labels[rand_image_id];
   images.erase(images.begin() + rand_image_id);
   labels.erase(labels.begin() + rand_image_id);
-  std::cout << "Actual class    = " << testLabel << '\n';
-  std::cout << " training...";
+  std::cout << "Actual class    = " << testLabel << '\n';*/
+  std::cout << " training..." << std::endl;
 
   cv::Ptr<cv::face::BasicFaceRecognizer> model = cv::face::EigenFaceRecognizer::create();
   model->train(images, labels);
-  int predictedLabel = model->predict(testSample);
-  std::cout << "\nPredicted class = " << predictedLabel << '\n';
+
+  cv::Mat frame;
+  double fps = 30;
+  const char win_name[] = "Face Recognition";
+  
+  cv::Mat gray_frame;
+  cv::Mat resize_frame;
+  cv::Size size(92, 112);
+
+  cv::VideoCapture vid_in(0);   // argument is the camera id
+  if (!vid_in.isOpened()) {
+      std::cout << "error: Camera 0 could not be opened for capture.\n";
+      return -1;
+  }
+  cv::namedWindow(win_name);
+
+  while (true) {
+      vid_in >> frame;
+      
+
+      cv::Rect myROI(250, 115, 180, 230);
+      cv::Mat cropped_frame = frame(myROI);
+
+      imshow(win_name, cropped_frame);
+
+      cvtColor(cropped_frame, gray_frame, CV_BGR2GRAY);
+      cv::resize(gray_frame, resize_frame, size);
+
+      int predictedLabel = model->predict(resize_frame);
+      std::cout << "\nPredicted class = " << predictedLabel << '\n';
+
+      if (cv::waitKeyEx(1000 / fps) >= 0) // how long to wait for a key (milliseconds)
+          break;
+  }
+
+  vid_in.release();
   return 0;
 }
